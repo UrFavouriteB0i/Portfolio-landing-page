dc4fc4f878feaa4c7ae51c5097f4cf68f0493c15
Zhilaan Rusmawan <zhilaanabdrsyd@gmail.com>
2024-11-01 19:41:22 +0700

    added responsive design

--- src/Component/Pro.jsx
+++ src/Component/Pro.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 export const Pro = () => {
   return (
     <div className='pb-10'>
-      <div className='flex flex-col items-center pt-20 lg:px-40 md:px-20 sm:px-0'>
+      <div className='flex flex-col items-center pt-20 xl:px-40 md:px-20 sm:px-0'>
         <h4>PROJECTS</h4>
         <h5 className='text-transparent bg-clip-text bg-gradient-to-r from-purple-900 from-10% via-orange-300 via-50% to-white to-80%'>DISCOVER NOW</h5>
         
-        <div className='container mx-auto px-4 lg:px-4'>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 gap-x-4 mx-20 mt-8'>
+        <div className='container mx-auto px-4 xl:px-6'>
+          <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-y-8 gap-x-4 xl:mx-20 mt-8'>
             {/* Project 1 */}
-            <div className='flex items-center ml-8 space-x-2'>
+            <div className='flex items-center space-x-2'>
               <img className='mix-blend-luminosity w-32 sm:w-40 md:w-56' src='./image 2.png' alt='EC Brain Detection' />
               <div className='font-InterBld text-sm sm:text-base w-full max-w-xs'>
                 <p>EC Brain Detection</p>
@@ -32,7 +32,7 @@ export const Pro = () => {
             </div>
 
             {/* Project 3 */}
-            <div className='flex items-center ml-8 space-x-2'>
+            <div className='flex items-center space-x-2'>
               <img className='mix-blend-luminosity w-32 sm:w-40 md:w-56' src='./image 4.png' alt='Defect Inspector' />
               <div className='font-InterBld text-sm sm:text-base w-full max-w-xs'>
                 <p>Defect Inspector</p>
@@ -56,7 +56,7 @@ export const Pro = () => {
         </div>
       </div>
       
-      <section className='container pt-10 px-4 flex flex-col items-center'>
+      <section className='container pt-10 px-10 mx-0 min-w-full flex flex-col items-center'>
         <a
           href="https://github.com/UrFavouriteB0i?tab=repositories"
           target="_blank"
